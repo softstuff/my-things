af3e1ba3344638c7f90e5b1e2c2fa1607cd3989f
Stefan <ztefan.lundgren@gmail.com>
2021-07-01 08:32:44 +0200

    remove and dispose private key

--- bin/emulator.bat
+++ bin/emulator.bat
@@ -5,7 +5,7 @@ set FIRESTORE_EMULATOR_HOST=localhost:8080
 set FIREBASE_AUTH_EMULATOR_HOST=localhost:9099
 set FUNCTIONS_EMULATOR_HOST=localhost:5001
 set NGROK_URL=https://ac1102ffa2b3.eu.ngrok.io
-set GOOGLE_APPLICATION_CREDENTIALS=C:\Users\Stefan\Dev\my-things\my-things-60357-4034a674c840.json
+set GOOGLE_APPLICATION_CREDENTIALS=C:\Users\Stefan\Dev\my-things\my-things-60357-c8e14fb674eb.json
 
 npm run emulator:debgger
 
