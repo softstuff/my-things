f00812edcbdd912f50e08925364c7e330cd3c1b3
Stefan Lundgren <ztefan.lundgren@gmail.com>
2021-07-11 00:03:10 +0200

    fixed

--- functions/imports/importer.js
+++ functions/imports/importer.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const csv = require('csv-parser');
 const { firestore, database } = require('../firebase-service');
-const { Transform, Stream } = require('stream');
+const { Transform, Stream, Writable } = require('stream');
 
 const logger = functions.logger
 
@@ -121,6 +121,143 @@ const runtimeOpts = {
   memory: '1GB'
 }
 
+const transformCsvToObject = ({importConfig}) => {
+  const registry = buildRegistry(importConfig.mapping)
+
+  return new Transform({
+    objectMode: true,
+    transform(data, encoding, next) {
+      try {
+        const { result } = processChunk(data, registry)
+        return next(null, result)
+      } catch(error) {
+        console.error(error)
+        return next(error, null)
+      }
+    }
+  })
+}
+
+const storeStream = ({collectionRef, maxBatchSize}) => {
+  let batch = firestore.batch();
+  let batchSize = 0
+  
+  const commit = async () => {
+    await batch.commit()
+    batch = admin.firestore().batch();
+    batchSize = 0;
+  }
+
+  return new Transform({
+    objectMode: true,
+    async transform(result, encoding, next) {
+      console.log("store stream chunk")
+      batch.set(collectionRef.doc(result.key), result.attributes)
+      if (++batchSize >= maxBatchSize) {
+        try{
+          logger.debug("Do batch commit")
+          await commit()
+          return next(null, result)
+        } catch(error) {
+          return next(error)
+        }
+      }
+      return next(null, result)
+    },
+    async final (next) {
+      if (batchSize > 0) {
+        try{
+          logger.debug("Do final batch commit")
+          await commit()
+        } catch(error) {
+          return next(error)
+        }
+      }
+      console.log("store stream done")
+      return next()
+    }  
+  })
+}
+
+const statStream = ({statRef, fileStatRef}) => {
+  let current = 0;
+  let perIteration = 0;
+  let rate = 1000
+  const startTime = Number(new Date())
+
+  const timer = setInterval(async () => { 
+    const now = perIteration
+    perIteration = 0
+    current += now
+
+    const time = Math.round((Number(new Date()) - startTime) / 1000)
+    
+    console.log(`Got ${now} last ${Math.round(rate/1000)} sec, total ${current} for the last ${time} sec`)
+    try{
+      await Promise.all([
+      statRef.update({ "imported": admin.database.ServerValue.increment(now) }),
+      fileStatRef.update({
+        "imported": admin.database.ServerValue.increment(now),
+        "timeSec": time,
+        "ratePerSec": current / time
+      })
+    ])
+    } catch(error) {
+      logger.warn("Failed to store stats", error)
+    }
+    
+  }, rate);
+
+  return new Transform({
+    objectMode: true,
+    transform(chunk, encoding, next) {
+      console.log("stat stream chunk")
+      perIteration++;
+      return next(null, chunk)
+    },
+    async final (next) {
+      console.log("stat stream done")
+      clearInterval(timer)
+      current += perIteration
+      console.log(`Got total ${current} for during ${ Math.round((Number(new Date()) - startTime) / 1000) } seconds`)
+
+      try {
+        await Promise.all([
+          statRef.update({ "imported": admin.database.ServerValue.increment(perIteration) }),
+          fileStatRef.update({
+            "imported": admin.database.ServerValue.increment(perIteration),
+            "endedAt": admin.database.ServerValue.TIMESTAMP
+          })
+        ])
+      } catch(error) {
+        logger.warn("Failed to store final stats", error)
+      }
+      return next()
+    }  
+  })
+}
+
+const delayStream = ({delayInMs}) => {  
+
+  delay = delayInMs => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(2);
+      }, delayInMs);
+    });
+  }
+
+  return new Transform({
+    objectMode: true,
+    async transform(chunk, encoding, next) {
+      console.log(`delay stream for ${delayInMs} ms`)
+      await delay(delayInMs)
+      return next(null, chunk)
+    }
+  })
+
+}
+
 exports.imports = functions
   // .runWith(runtimeOpts)
   .storage
@@ -136,8 +273,8 @@ exports.imports = functions
 
       const workspaceSnap = await firestore.doc(workspaceDocPath).get()
       const importConfig = workspaceSnap.get(`imports.${iid}`)
-
-      const registry = buildRegistry(importConfig.mapping)
+      
+      
 
       
       const fileName = file.name.split("/")[4]
@@ -168,120 +305,114 @@ exports.imports = functions
         imported: 0
       })
 
-      console.log("AAA")
       const maxBatchSize = 500;
       let importedFromFile = 0;
       const started = Number(new Date())
       let batchSize = 0;
       let batch = firestore.batch();
       const collection = "Person"
-      const colRef = firestore.collection(`/tenants/${tenantId}/workspaces/${wid}/${collection}`)
-      const dataStream = file.createReadStream()
+      const collectionRef = firestore.collection(`/tenants/${tenantId}/workspaces/${wid}/${collection}`)
+      const validation = process.env.NODE_ENV === 'development' ? false : 'crc32c'
+      const dataStream = file.createReadStream({validation})
       const parsePromise = new Promise((resolve, reject) => {
 
         dataStream
+          
           .pipe(csv(optionsOrHeaders = {
             separator: importConfig.config.separator,
             skipComments: true,
-          }
+          }))
+          .pipe(delayStream({delayInMs: 500}))
+          .pipe(transformCsvToObject({importConfig}))
+          .pipe(storeStream({collectionRef, maxBatchSize}))
+          .pipe(statStream({statRef, fileStatRef}))
+          
+          // .pipe(new Transform({
+          //   objectMode: true,
+          //   transform(result, encoding, next) {
 
-          ))
-          .pipe(new Transform({
-            objectMode: true,
-            transform(data, encoding, next) {
-              console.log("BB")
-              const { result } = processChunk(data, registry)
+          //     batch.set(colRef.doc(result.key), result.attributes)
+          //     importedFromFile++
+          //     const time = Number(new Date()) - started
+          //     if (++batchSize >= maxBatchSize) {
 
-              batch.set(colRef.doc(result.key), result.attributes)
-              importedFromFile++
-              console.log("CC")
-              const time = Number(new Date()) - started
-              console.log("DD")
-              if (++batchSize >= maxBatchSize) {
+          //       logger.debug("Do batch commit")
+          //       return Promise.all([
+          //         statRef.update({ "imported": admin.database.ServerValue.increment(batchSize) }),
+          //         fileStatRef.update({
+          //           "imported": admin.database.ServerValue.increment(batchSize),
+          //           "timeMs": time,
+          //           "ratePerSec": importedFromFile / (time / 1000)
+          //         }),
+          //         batch.commit()
+          //           .then((result) => {
+          //             batch = admin.firestore().batch();
+          //             batchSize = 0;
 
-                logger.debug("Do batch commit")
-                Promise.all([
-                  statRef.update({ "imported": admin.database.ServerValue.increment(batchSize) }),
-                  fileStatRef.update({
-                    "imported": admin.database.ServerValue.increment(batchSize),
-                    "timeMs": time,
-                    "ratePerSec": importedFromFile / (time / 1000)
-                  }),
-                  batch.commit()
-                    .then((result) => {
-                      batch = admin.firestore().batch();
-                      batchSize = 0;
-
-                      logger.debug("Batch commit is done, pipe call, ", result.writeTime)
-                      console.log("Next A")
-                      return result
-                    })
-                    .catch(error => {
-                      console.log("Error A", error)
-                      next(error, data)
-                    })
-                ])
-                  .then(() => {
-                    console.log("Next B")
-                    return next(null, data)
-                  })
-                  .catch(error => {
-                    console.log("Error B", error)
-                    return next(error)
-                  })
+          //             logger.debug("Batch commit is done")
+          //             return result
+          //           })
+          //           .catch(error => {
+          //             next(error)
+          //           })
+          //       ])
+          //         .then(() => {
+          //           return next(null, result)
+          //         })
+          //         .catch(error => {
+          //           return next(error)
+          //         })
                   
-              } else {
-                console.log("Next C")
-                next(null, data)
-              }
-            },
-            final(next) {
-              console.log("final")
-              if (batchSize > 0) {
-                logger.debug("pipe final, batchSize", batchSize, " wait for final batch commit")
-                Promise.all([
-                  statRef.update({ "imported": admin.database.ServerValue.increment(batchSize) }),
-                  fileStatRef.update({
-                    "imported": admin.database.ServerValue.increment(batchSize),
-                    "endedAt": admin.database.ServerValue.TIMESTAMP
-                  }),
-                  batch.commit()
-                    .then(() => {
-                      logger.debug("Batch commit is done, pipe is done")
-                      return
-                    })
-                    .catch(error => {
-                      console.log("Next Error E", error)
-                      next(error)
-                    })
-                ])
-                .then(() => {
-                  logger.debug("final next")
-                  return next()
-                })
-                .catch(error => {
-                  console.log("Next Error E", error)
-                  next(error)
-                })
+          //     } else {
+          //       return next(null, result)
+          //     }
+          //   },
+          //   final(next) {
+          //     if (batchSize > 0) {
+          //       logger.debug("pipe final, batchSize", batchSize, " wait for final batch commit")
+          //       return Promise.all([
+          //         statRef.update({ "imported": admin.database.ServerValue.increment(batchSize) }),
+          //         fileStatRef.update({
+          //           "imported": admin.database.ServerValue.increment(batchSize),
+          //           "endedAt": admin.database.ServerValue.TIMESTAMP
+          //         }),
+          //         batch.commit()
+          //           .then(() => {
+          //             logger.debug("Batch commit is done, pipe is done")
+          //             return
+          //           })
+          //           .catch(error => {
+          //             console.log("Next Error E", error)
+          //             next(error)
+          //           })
+          //       ])
+          //       .then(() => {
+          //         logger.debug("final next")
+          //         return next()
+          //       })
+          //       .catch(error => {
+          //         console.log("Next Error E", error)
+          //         next(error)
+          //       })
 
 
-              } else {
-                logger.debug("Pipe is done")
-                fileStatRef.update({
-                  "endedAt": admin.database.ServerValue.TIMESTAMP
-                })
-                .then(() => {
-                  logger.debug("final next")
-                  return next()
-                })
-                .catch(error => {
-                  next(error)
-                })
-              }
-            },
-          }
+          //     } else {
+          //       logger.debug("Pipe is done")
+          //       return fileStatRef.update({
+          //         "endedAt": admin.database.ServerValue.TIMESTAMP
+          //       })
+          //       .then(() => {
+          //         logger.debug("final next")
+          //         return next()
+          //       })
+          //       .catch(error => {
+          //         next(error)
+          //       })
+          //     }
+          //   },
+          // }
 
-          ))
+          // ))
           .on("error", err => {
             logger.error("import faield", err)
             reject(err)
